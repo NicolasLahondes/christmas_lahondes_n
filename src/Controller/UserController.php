b8fd8e4fde9797bd1b1c35edb9042d3dd52e6daf
Lahondès <lahondes.n@gmail.com>
2021-11-23 14:07:16 +0100

    added user areas

---
+++ src/Controller/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/u_article")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/allArticles", name="u_allarticles")
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+             'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="u_article_show", methods={"GET"})
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('user/u_show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+}
