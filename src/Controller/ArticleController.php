a94e2f4a9a3c3db271cd1f23ff3887376e27895f
Lahondès <lahondes.n@gmail.com>
2021-11-23 12:45:30 +0100

    added all images and pdf upload and modifications

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/article")
@@ -22,6 +25,7 @@ class ArticleController extends AbstractController
      */
     public function index(ArticleRepository $articleRepository): Response
     {
+
         return $this->render('article/index.html.twig', [
             'articles' => $articleRepository->findAll(),
         ]);
@@ -30,25 +34,129 @@ class ArticleController extends AbstractController
     /**
      * @Route("/new", name="article_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator, SluggerInterface $slugger): Response
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($article);
-            $entityManager->flush();
+        if ($form->isSubmitted()) {
+            // Error management
+            $errors = $validator->validate($article);
+            if (count($errors) > 0) {
+                return $this->render('author/validation.html.twig', [
+                    'errors' => $errors,
+                ]);
+            }
+            // image modification/upload management
+            if (!empty($form->get('picturemain'))) {
+                /** @var UploadedFile $imagesFile */
+                $imagesFile = $form->get('picturemain')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($imagesFile) {
+                    $originalFilename = pathinfo($imagesFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imagesFile->guessExtension();
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $imagesFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $article->setPicturemain($newFilename);
+                }
+            }
+            if (!empty($form->get('picturefront'))) {
+                /** @var UploadedFile $imagesFile */
+                $imagesFile = $form->get('picturefront')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($imagesFile) {
+                    $originalFilename = pathinfo($imagesFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imagesFile->guessExtension();
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $imagesFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $article->setPicturefront($newFilename);
+                }
+            }
+            if (!empty($form->get('pictureback'))) {
+                /** @var UploadedFile $imagesFile */
+                $imagesFile = $form->get('pictureback')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($imagesFile) {
+                    $originalFilename = pathinfo($imagesFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imagesFile->guessExtension();
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $imagesFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $article->setPictureback($newFilename);
+                }
+            }
+            if (!empty($form->get('manual')))
+                $brochureFile = $form->get('manual')->getData();
 
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $article->setManual($newFilename);
+            }
+            if ($form->isValid()) {
+                $entityManager->persist($article);
+                $entityManager->flush();
+            }
             return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
         }
-
         return $this->renderForm('article/new.html.twig', [
             'article' => $article,
             'form' => $form,
         ]);
     }
-
     /**
      * @Route("/{id}", name="article_show", methods={"GET"})
      */
@@ -62,19 +170,120 @@ class ArticleController extends AbstractController
     /**
      * @Route("/{id}/edit", name="article_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Article $article, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
+    public function edit(Request $request, Article $article, EntityManagerInterface $entityManager, ValidatorInterface $validator, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
 
+            // Error management
             $errors = $validator->validate($article);
             if (count($errors) > 0) {
                 return $this->render('author/validation.html.twig', [
                     'errors' => $errors,
                 ]);
             }
+
+            // image modification/upload management
+            if (!empty($form->get('picturemain'))) {
+                /** @var UploadedFile $imagesFile */
+                $imagesFile = $form->get('picturemain')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($imagesFile) {
+                    $originalFilename = pathinfo($imagesFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imagesFile->guessExtension();
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $imagesFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $article->setPicturemain($newFilename);
+                }
+            }
+            if (!empty($form->get('picturefront'))) {
+                /** @var UploadedFile $imagesFile */
+                $imagesFile = $form->get('picturefront')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($imagesFile) {
+                    $originalFilename = pathinfo($imagesFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imagesFile->guessExtension();
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $imagesFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $article->setPicturefront($newFilename);
+                }
+            }
+            if (!empty($form->get('pictureback'))) {
+                /** @var UploadedFile $imagesFile */
+                $imagesFile = $form->get('pictureback')->getData();
+                // this condition is needed because the 'brochure' field is not required
+                // so the PDF file must be processed only when a file is uploaded
+                if ($imagesFile) {
+                    $originalFilename = pathinfo($imagesFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imagesFile->guessExtension();
+                    // Move the file to the directory where brochures are stored
+                    try {
+                        $imagesFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                    }
+                    // updates the 'brochureFilename' property to store the PDF file name
+                    // instead of its contents
+                    $article->setPictureback($newFilename);
+                }
+            }
+            if (!empty($form->get('manual')))
+                $brochureFile = $form->get('manual')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $article->setManual($newFilename);
+            }
+
             if ($form->isValid()) {
                 $entityManager->flush();
                 return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,9 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ArticleType extends AbstractType
 {
@@ -18,11 +23,88 @@ class ArticleType extends AbstractType
             ->add('description')
             ->add('price')
             ->add('stock')
-            ->add('picturemain')
-            ->add('picturefront')
-            ->add('pictureback')
-            ->add('manual')
-        ;
+            ->add('picturemain', FileType::class, [
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif', 'image/x'],
+                        'mimeTypesMessage' => 'Please upload a valid document',
+                    ])
+                ],
+            ])
+            ->add('picturefront', FileType::class, [
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif', 'image/x'],
+                        'mimeTypesMessage' => 'Please upload a valid document',
+                    ])
+                ],
+            ])
+            ->add('pictureback', FileType::class, [
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif', 'image/x'],
+                        'mimeTypesMessage' => 'Please upload a valid document',
+                    ])
+                ],
+            ])
+            ->add('manual', FileType::class, [
+                'label' => 'Brochure (PDF file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('fk_category', EntityType::class, array('class' => Category::class, 'choice_label' => 'name', 'expanded' => true))
+            ->add('fk_tags', EntityType::class, array('class' => Tag::class, 'choice_label' => 'name', 'multiple' => true, 'expanded' => true));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
